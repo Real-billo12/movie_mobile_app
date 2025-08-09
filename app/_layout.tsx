bb09ae3324d788351058e05f82f1761150dbd1b0
hollali <dheztinykartel@gmail.com>
2025-08-09 16:29:55 +0000

    `Added StatusBar and modified RootLayout in _layout.tsx, removed index.tsx`

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,12 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 import './global.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+    <StatusBar hidden={true}/>
+    <Stack/>
+    </>
+  )
 }
