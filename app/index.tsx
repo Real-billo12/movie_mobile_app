33d06e78e1084d55de3d3531d183abe39374e86f
hollali <dheztinykartel@gmail.com>
2025-08-09 15:37:08 +0000

    `Update Expo Router and Metro Config, remove unused imports and Tailwind CSS`

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,9 @@
-import {
-  Text,
-  View,
-} from "react-native";
+import { Text, View } from "react-native";
 import { useRouter } from "expo-router";
-export default function index(){
-  return ( 
-    
-          <View className="flex-1 justify-center item-center">
-            <Text className="text-5xl text-primary
-             font-bold ">welcome</Text>
-    </View>
-  );
+export default function index() {
+	return (
+		<View className="flex-1 justify-center item-center">
+			<Text className="text-5xl text-primary font-bold ">welcome</Text>
+		</View>
+	);
 }
