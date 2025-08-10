030b712ce790e67834c61cd1ad9c6e0305f4fdf8
Real-billo12 <billionfrancis525@gmail.com>
2025-08-10 01:28:13 -0700

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import {
+  Text,
+  View,
+} from "react-native";
+import { useRouter } from "expo-router";
+export default function index(){
+  return ( 
+    
+          <View className="flex-1 justify-center item-center">
+            <Text className="text-5xl text-primary
+             font-bold ">welcome</Text>
+    </View>
+  );
+}
