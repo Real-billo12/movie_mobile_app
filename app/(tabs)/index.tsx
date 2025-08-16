436677ca72cba015910db07c0aa024f46f7843ed
hollali <dheztinykartel@gmail.com>
2025-08-16 15:14:39 +0000

    `Added logo, search bar, and scroll view to tabs page`

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,26 @@
-
 import SearchBar from "@/components/searchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { Image, ScrollView, View } from "react-native";
 
-
-
 const Index = () => {
 	return (
 		<View className="flex-1 bg-primary">
-      <Image  
-        source={images.bg}
-        className="absolute w-full z-0"
-        resizeMode="cover"
-      />
-	  </View>
+			<Image
+				source={images.bg}
+				className="absolute w-full z-0"
+				resizeMode="cover"
+			/>
+			<ScrollView
+				className="flex -1 px-5"
+				showsVerticalScrollIndicator={false}
+				contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
+				<Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+				<view className="flex-1 mt-5">
+					<SearchBar />
+				</view>
+			</ScrollView>
+		</View>
 	);
 };
 export default Index;
